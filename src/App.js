6563ce2ec9df853b6e538d1c6d7b6ffebf4d8bdc
Leomar Alejar <leomaramieldev@gmail.com>
2018-03-14 23:52:40 +0800

    Add WIP Header

--- src/App.js
+++ src/App.js
@@ -32,12 +32,9 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Homepage/>
       </Wrapper>
     );
